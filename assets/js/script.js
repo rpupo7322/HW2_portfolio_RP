a56f865c1a25a4713c5ce5b83dc6dcbd875525f6
Rob Pupo <pupo.robert@gmail.com>
2021-04-10 22:01:11 -0400

    including javascript, some more content (about me), and various comments

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+// adding coming soon alert boxes to the applications which haven't been made yet (all of them)
+document.getElementById("app1").addEventListener("click", function(){
+    alert('Coming Soon!');
+});
+document.getElementById("app2").addEventListener("click", function(){
+    alert('Coming Soon!');
+});
+document.getElementById("app3").addEventListener("click", function(){
+    alert('Coming Soon!');
+});
+document.getElementById("app4").addEventListener("click", function(){
+    alert('Coming Soon!');
+});
+document.getElementById("app5").addEventListener("click", function(){
+    alert('Coming Soon!');
+});
+
+// adding coming soon alert box to the resume, since it doesn't exist yet
+document.getElementById("resume").addEventListener("click", function(){
+    alert('Coming Soon!');
+})
+
